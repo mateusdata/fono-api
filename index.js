11b849b7a4988e4b0355bc89330cbb455452e675
Mateus Santos <mateuspele2015@gmail.com>
2023-11-11 22:09:17 -0300

    fix: vercel.json

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+const sequelize = require('./src/Config/Databases/Database');
 const app = express();
-const port = 3001;
+const port = 3000;
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-
-app.get('/',  (req, res) => {
-  res.send({ fono: "fono api" });
+app.get('/users', async (req, res) => {
+  try {
+    const usuarios = await sequelize.query('SELECT * FROM usuarios');
+    res.send(usuarios);
+  } catch (error) {
+    console.error('Erro ao buscar usuários:', error);
+    res.status(500).send({ error: 'Erro interno do servidor' });
+  }
 });
+
+app.get('/', async (req, res) => {
+  res.send({fono:"fono api"});
+});
+
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
