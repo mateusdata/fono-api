8232980d3ae5788e6d6ac9719a50a7a9bda49c72
Mateus Santos <mateuspele2015@gmail.com>
2023-11-11 22:04:28 -0300

    fix: remove async

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const sequelize = require('./src/Config/Databases/Database');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.json());
 app.use(cors());
-app.get('/users', async (req, res) => {
-  try {
-    const usuarios = await sequelize.query('SELECT * FROM usuarios');
-    res.send(usuarios);
-  } catch (error) {
-    console.error('Erro ao buscar usuários:', error);
-    res.status(500).send({ error: 'Erro interno do servidor' });
-  }
-});
+
 
 app.get('/',  (req, res) => {
   res.send({ fono: "fono api" });
