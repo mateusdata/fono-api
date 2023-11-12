05f33f7fb5f4019e06c10acd05c4856e0821782d
Mateus Santos <mateuspele2015@gmail.com>
2023-11-11 21:33:27 -0300

    added: new feature

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const { QueryTypes } = require('sequelize');
 const sequelize = require('./src/Config/Databases/Database');
 const app = express();
 const port = 3000;
@@ -8,17 +7,18 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', async (req, res) => {
+app.get('/users', async (req, res) => {
   try {
-    const usuarios = await sequelize.query('SELECT * FROM usuarios', {
-      type: QueryTypes.SELECT,
-    });
-
-    res.json(usuarios);
+    const usuarios = await sequelize.query('SELECT * FROM usuarios');
+    res.send(usuarios);
   } catch (error) {
     console.error('Erro ao buscar usuários:', error);
-    res.status(500).json({ error: 'Erro interno do servidor' });
+    res.status(500).send({ error: 'Erro interno do servidor' });
   }
+});
+
+app.get('/', async (req, res) => {
+  res.send({fono:"fono api"});
 });
 
 app.listen(port, () => {
