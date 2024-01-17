1f1d2086b361772b3453101dce49d40b8b5f00e7
FelipeMaced0 <fmacedo746@gmail.com>
2024-01-17 00:36:46 -0300

    feat: exercise plan controller and model

---
+++ src/models/ExercisePlan.js
@@ -0,0 +1,49 @@
+const  { Model, DataTypes } = require('sequelize');
+const sequelize = require("../config/sequelize");
+const Exercise = require("./Exercise");
+
+class ExercisePlan extends Model {}
+
+ExercisePlan.init({
+    exp_id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    exe_id:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references:{
+            model: "Exercise",
+            key: "exe_id"
+        }
+    },
+    repetitions:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    series:{
+        type: DataTypes.INTEGER,
+        allowNull:false
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        allowNull: true,
+    },
+    updated_at: {
+        type: DataTypes.DATE,
+        allowNull: true,
+    },
+}, {
+    sequelize,
+    modelName: 'ExercisePlan',
+    tableName: 'exercise_plan',
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+});
+
+ExercisePlan.hasOne(Exercise);
+Exercise.belongsTo(ExercisePlan);
+
+module.exports = ExercisePlan;
