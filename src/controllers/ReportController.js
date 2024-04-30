84498bf061510aa96a1cac31f15761b4372bf2c4
FelipeMaced0 <fmacedo746@gmail.com>
2024-04-29 21:47:29 -0300

    feat: adding geo lite back for testing, previous error as due to another factor

--- src/controllers/ReportController.js
+++ src/controllers/ReportController.js
@@ -8,7 +8,7 @@ const Answer = require('../models/Answer');
 const Section = require('../models/Section');
 const S = require('string');
 var locale_pt_br = require('dayjs/locale/pt-br');
-
+const geoIp2 = require('geoip-lite2');
 const User = require('../models/User');
 const Doctor = require('../models/Doctor');
 const whois = require('../services/IdentityService');
@@ -173,7 +173,7 @@ class ReportController {
 
             const serviceOptions = serviceSchema.parse(req.body);
 
-            //const geoIpLocation = geoIp2.lookup(req.ip);
+            const geoIpLocation = geoIp2.lookup(req.ip);
             const user = await User.findByPk(whois(req), { include: [Doctor, Person] });
 
             const pacient = await Pacient.findByPk(req.params.id, {
@@ -229,7 +229,7 @@ class ReportController {
             }
 
             const dateInfo = {
-                city_name: '____________________________',
+                city_name: geoIpLocation?.city || '____________________________',
                 day: dayjs().format('DD'),
                 month_name: S(dayjs().locale(locale_pt_br).format('MMMM')).capitalize(),
                 year: dayjs().format('YYYY')
@@ -426,7 +426,7 @@ class ReportController {
             const DischargeReport = DischargeReportSchema.parse(req.body);
             const user = await User.findByPk(whois(req), { include: [Doctor, Person] });
             const pacient = await Pacient.findByPk(req.params.pac_id, { include: Person });
-            //const geoIpLocation = geoIp2.lookup(req.ip);
+            const geoIpLocation = geoIp2.lookup(req.ip);
 
             if (!pacient) {
                 return res.status(404).send({ message: 'Pacient has not been found' });
@@ -487,7 +487,7 @@ class ReportController {
 
             const cityTemplate = `{{city_name}}, {{day}} de {{month_name}} de {{year}}`;
             const cityData = {
-                city_name: '_______________________',
+                city_name: geoIpLocation?.city || '_______________________',
                 day: dayjs().format('DD'),
                 month_name: S(dayjs().locale(locale_pt_br).format('MMMM')).capitalize().toString(),
                 year: dayjs().format('YYYY')
