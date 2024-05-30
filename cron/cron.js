8b2c700ae4df87745a32ad46e3c82239590fdaff
FelipeMaced0 <fmacedo746@gmail.com>
2024-05-30 11:13:54 -0300

    feat: adding public directory and videos

--- cron/cron.js
+++ cron/cron.js
@@ -1,9 +1,11 @@
 const cron = require('node-cron');
+const os = require('os');
+
 const { exec } = require('child_process');
 
 cron.schedule("2 * * * * *", () => {
 
-    exec("find pdfs -type f -name \"*pdf\" -mmin +2 -delete", (error, stdout, stderr) => {
+    exec(`find ${os.temp()} -type f -name \"*pdf\" -mmin +2 -delete`, (error, stdout, stderr) => {
       if (error) {
         return;
       }
